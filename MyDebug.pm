790410b08c0b6165e644e503495e7e4ff3eaf187
Scott C Edwards <supaplextor@users.noreply.github.com>
2025-11-18 13:47:47 -0700

    debugging

---
+++ MyDebug.pm
@@ -0,0 +1,13 @@
+# MyDebug.pm
+package MyDebug;
+
+sub DB::get_fork_TTY {
+    # Open a new xterm, print its tty to stdout, and sleep to keep it open
+    open my $xt, q[3>&1 | /usr/bin/xterm -title 'Forked Perl debugger' -e sh -c 'tty 1>&3; sleep 10000000'] or die "Cannot open xterm: $!";
+    my $tty = <$xt>;
+    chomp $tty if defined $tty;
+    $DB::fork_TTY = $tty;
+    return $tty;
+}
+
+1;
